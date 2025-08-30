b6db61dc637e537c2a892741bfb31408d153cd52
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-30 18:48:07 +0200

    added form

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -2,10 +2,18 @@ function ApiLinkSection() {
   return (
     <>
       <div className="rounded-div">
-        <p>ApiLinkSection</p>
+        <form action="/submit" method="GET">
+          <select name="options" id="requst-dropdown">
+            <option value="GET">GET</option>
+            <option value="POST">POST</option>
+            <option value="PUT">PUT</option>
+            <option value="DELETE">DELETE</option>
+          </select>
+
+          <input type="url" name="requestUrl" id="request-input" />
+          <button type="submit">SEND</button>
+        </form>
       </div>
-      
-      
     </>
   );
 }
