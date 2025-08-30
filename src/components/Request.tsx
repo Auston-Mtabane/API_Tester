a3ea3dece880fc10c64bd785f1d44c4082c85cbc
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-30 19:11:58 +0200

    styling 1

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,20 +1,18 @@
 function ApiLinkSection() {
   return (
-    <>
       <div className="rounded-div">
-        <form action="/submit" method="GET">
+        <form action="/submit" method="GET"  className="form">
           <select name="options" id="requst-dropdown">
-            <option value="GET">GET</option>
-            <option value="POST">POST</option>
-            <option value="PUT">PUT</option>
-            <option value="DELETE">DELETE</option>
+            <option value="GET" id="GET">GET</option>
+            <option value="POST" id="POST">POST</option>
+            <option value="PUT" id="PUT">PUT</option>
+            <option value="DELETE" id="DELETE">DELETE</option>
           </select>
 
           <input type="url" name="requestUrl" id="request-input" />
           <button type="submit">SEND</button>
         </form>
       </div>
-    </>
   );
 }
 
