8290d2ce659aa920053ef1e3e13f0dd76ac70abc
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-31 21:26:50 +0200

    can make get request

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,15 +1,34 @@
 import { useState } from "react";
 import '../styles/App.css';
 
+async function sendReq (url:string): Promise<void> {
+  const method = "GET"
+  console.log(`Sending ${method} request to ${url}`);
+
+    try {
+      const response = await fetch(url, { method });
+      const data = await response.json();
+      console.log("Response:", data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+}
 function ApiLinkSection() {
   const [classname,setClassname] = useState("GET");
+  const [url,setUrl] = useState("");
   const handleChange = (event:React.ChangeEvent<HTMLSelectElement>) =>{
     const method = event.target.value;
-    setClassname(method)
+    setClassname(method);
   }
+ 
+  const handleSubmit = async () =>{
+    sendReq(url);
+  }
+
+  
   return (
       <div className="rounded-div">
-        <form action="/submit" method="GET"  className="form">
+        <div className="form">
           <select name="options" id="requst-dropdown" onChange={handleChange} className={classname}>
             <option value="GET" id="GET" >GET</option>
             <option value="POST" id="POST" >POST</option>
@@ -17,9 +36,9 @@ function ApiLinkSection() {
             <option value="DELETE" id="DELETE">DELETE</option>
           </select>
 
-          <input type="url" name="requestUrl" id="request-input" placeholder="https://catfact.ninja/fact" />
-          <button type="submit">SEND</button>
-        </form>
+          <input type="url" name="requestUrl" id="request-input" placeholder="https://catfact.ninja/fact" onChange={(event)=>{setUrl(event.target.value)}} />
+          <button type="submit" onClick={handleSubmit}>SEND</button>
+        </div>
       </div>
   );
 }
