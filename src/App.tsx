aa9f997337dbd69e70f484896386ff7cc06a2b63
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-31 20:52:18 +0200

    drop down color

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
 
   return (
     <>
+    <h1>Test Your APIs</h1>
+    <p>on a simpler UI and a nice summarized stats and performance of your APIs</p>
     <Request />
     <MetaData />
     <Response />
--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+import '../styles/App.css';
+
 function ApiLinkSection() {
+  const [classname,setClassname] = useState("GET");
+  const handleChange = (event:React.ChangeEvent<HTMLSelectElement>) =>{
+    const method = event.target.value;
+    setClassname(method)
+  }
   return (
       <div className="rounded-div">
         <form action="/submit" method="GET"  className="form">
-          <select name="options" id="requst-dropdown">
-            <option value="GET" id="GET">GET</option>
-            <option value="POST" id="POST">POST</option>
+          <select name="options" id="requst-dropdown" onChange={handleChange} className={classname}>
+            <option value="GET" id="GET" >GET</option>
+            <option value="POST" id="POST" >POST</option>
             <option value="PUT" id="PUT">PUT</option>
             <option value="DELETE" id="DELETE">DELETE</option>
           </select>
 
-          <input type="url" name="requestUrl" id="request-input" placeholder="https://catfact.ninja/fact
-" />
+          <input type="url" name="requestUrl" id="request-input" placeholder="https://catfact.ninja/fact" />
           <button type="submit">SEND</button>
         </form>
       </div>
