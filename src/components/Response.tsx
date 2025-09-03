ac084fce4d4310e15bfdd900ff71c75b470f90f4
Auston Mtabane <mtabane.auston@gmail.com>
2025-09-03 13:18:18 +0200

    can style color

--- src/components/Response.tsx
+++ src/components/Response.tsx
@@ -1,9 +1,8 @@
-import "../styles/styleJson.css";
 import { useEffect, useRef } from "react";
 import hljs from "highlight.js/lib/core";
 import json from "highlight.js/lib/languages/json";
 import "highlight.js/styles/github-dark.css";
-
+import "../styles/styleJson.css";
 hljs.registerLanguage("json", json);
 
 interface Resp {
@@ -11,30 +10,30 @@ interface Resp {
 }
 
 function Response({ data }: Resp) {
-  let g = {};
-  if (data != "") {
+  let g = { message: "Welcome to Auston's API Tester", version: "^1.0.0" };
+  if (data !== "") {
     g = JSON.parse(data);
   }
-  console.log(JSON.stringify(g, null, 2));
 
   const codeRef = useRef<HTMLElement>(null);
+
   useEffect(() => {
     if (codeRef.current) {
-      hljs.highlightElement(codeRef.current);
+      const formatted = JSON.stringify(g, null, 2);
+      const highlighted = hljs.highlight(formatted, { language: "json" }).value;
+      codeRef.current.innerHTML = highlighted; // inject HTML with colors
     }
   }, [data]);
 
   return (
-    <>
-      <div id="response">
-        <div id="title-div">
-          <h4>Response: </h4>
-        </div>
-        <pre>
-          <code ref={codeRef} className="language-json">{JSON.stringify(g,null,2)}</code>
-        </pre>
+    <div id="response">
+      <div id="title-div">
+        <h4>Response:</h4>
       </div>
-    </>
+      <pre >
+        <code ref={codeRef} className="language-json" />
+      </pre>
+    </div>
   );
 }
 
