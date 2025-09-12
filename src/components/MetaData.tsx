4442e5324f465164382e76d6070f90b6a63c5e88
Auston Mtabane <mtabane.auston@gmail.com>
2025-09-12 11:48:24 +0200

    meta data

--- src/components/MetaData.tsx
+++ src/components/MetaData.tsx
@@ -1,7 +1,21 @@
+import '../styles/metadata.css'
+import { useState } from "react";
+
 export default function MetaData() {
+  const [status,seStatus] = useState('OK');
   return (
     <>
-      <div id="meta-data"></div>
+      <div id="meta-data">
+        <span id="http-status" className="">
+        {status} status
+        </span>
+        <span id="speed" className="">
+        {678} ms
+        </span>
+        <span id="size" className="">
+        {1025} kb
+        </span>
+      </div>
     </>
   );
 }
