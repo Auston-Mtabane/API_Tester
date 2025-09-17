e22cd84eb6d9cc3dc1dd54973a75718a1787308c
Auston Mtabane <mtabane.auston@gmail.com>
2025-09-17 15:22:34 +0200

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "./styles/App.css";
 import Request from "./components/Request";
 import Response from "./components/Response";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 function App() {
-
   const [responseData, setResponseData] = useState<string>("");
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
     <>
@@ -14,6 +21,9 @@ function App() {
         <source src="/background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <button onClick={toggleTheme}>
+        Switch to {theme === "dark" ? "Light" : "Dark"} Mode
+      </button>
       <div id="container">
         <h1>Test Your APIs</h1>
         <p>
